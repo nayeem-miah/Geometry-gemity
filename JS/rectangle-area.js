f4df093042b507838e61af5be495a45b15bb9487
nayeem-miah <nayeem5113a@gmail.com>
2024-02-13 09:36:20 +0600

    rectrangle added

--- JS/rectangle-area.js
+++ JS/rectangle-area.js
@@ -1,4 +1,25 @@
 
 function calculatorRectTangleArea(){
-    console.log('what is error in my code');
+    // get length of the rectangle
+    const lengthInput=document.getElementById('rectangle-length');
+    const lengthText=lengthInput.value;
+    const length=parseFloat(lengthText);
+    console.log(length);
+
+    // get width of the rectangle
+    const widthInput=document.getElementById('rectangle-width');
+    const widthText=widthInput.value;
+    const width=parseFloat(widthText)
+    
+    console.log(width)
+
+
+    // calculate rectangle area
+    const area=length * width;
+    console.log('area of the rectangle is:',area)
+
+
+    // display rectangle area 
+    const rectangleAreaSpan=document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText=area;
 }
